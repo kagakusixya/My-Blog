6e4852765b8ad98b4cc0f66cf7e49a0adc893377
kagakusixya <kagakusixya@pumpkin.Pumpkin>
2018-06-09 01:42:05 -1000

    iroiro

--- go_framework/app/controllers/writecontrollers.go
+++ go_framework/app/controllers/writecontrollers.go
@@ -7,7 +7,7 @@ func (t *T) Preview_post() {
 	t.Html_data = map[string]string{
 		"main": t.Form["main"],
 		//"side": "<p><a href=\"/teapot\">・紅茶でも飲みませんか？</a></p>",
-		"side": t.Form["side"],
+		"side": "<p><a href=\"/teapot\">" + t.Form["side"] + "</a></p>",
 	}
 	t.Url = "burogu.html"
 }
